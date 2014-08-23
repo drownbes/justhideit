24cac47c5d193d5a55b843f8e84f859112d8834b
Markov Artem <drownbes@gmail.com>
2014-08-23 10:02:45 +0600

    Best version of options page. For a moment..

--- app/options.js
+++ app/options.js
@@ -1,4 +1,3 @@
-//TODO:di inject
 (function() {
 
 	function Elm(elm, opt) {
@@ -18,71 +17,44 @@
 		return el;
 	}
 
-	function Btn(opt) {
-		var id = opt.id || '';
-			cls = opt.cls || '';
-			label = opt.label || '';
-		var el = Elm('a', id, cls);
-		el.href = 'jav;void 0';
-		el.innerHTML = label;
-		return el;
-	}
-
-	function Input(opt) {
-		var id = opt.id || '';
-			cls = opt.cls || '';
-			label = opt.label || '';
-		var el = Elm('input',id,cls);
-		el.placeholder = label;
-		return el;
-	}
-
-	function alarm(elm) {
-		elm.classList.add('glowing-border');
+	function flash_class(elm,cls) {
+		elm.classList.add(cls);
 		setTimeout(function() {
-			elm.classList.remove('glowing-border');
+			elm.classList.remove(cls);
 		},1000);
 	}
 
-	function ok_alarm(elm) {
-		console.log("ok_alarm");
-		elm.classList.add('glowing-border-green');
-		setTimeout(function() {
-			elm.classList.remove('glowing-border-green');
-		},1000);
+	function flash_red(elm) {
+		flash_class(elm,'glow-red');
+	}
+
+	function flash_green(elm) {
+		flash_class(elm,'glow-green');
 	}
 
 	function ById(id) {
 		return document.getElementById(id)
 	}
 
-	function restore_options(callback) {
-		chrome.storage.sync.get({ options: '[]'}, function(o) {
-			var opt = JSON.parse(o.options);
-			if(!opt) opt = [];
-			callback(opt);
-		});
+	function display_none(elm) {
+		elm.style.display = 'none';
 	}
 
-	function save_options(opt, callback) {
-		chrome.storage.sync.set({ options: opt}, function() {
-			callback();
-		});
+	function display_block(elm) {
+		elm.style.display = 'block';
 	}
 
-	function onsave(opt) {
-		console.log("saved");//TODO: make user respose
-		chrome.runtime.sendMessage({cmd: "refresh_options", opt: str}, function(response) {
-		});
-	}
-
-	function ajax(url, callback) {
+	function ajax(url, success, fail) {
+		console.log("ajax", url, success, fail);
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = function() {
 			if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 				var resp = JSON.parse(xmlhttp.responseText);
 				if(resp) {
-					callback(resp);
+					success(resp);
+				}
+				else {
+					fail(xmlhttp.statusText);
 				}
 			}
 		}
@@ -90,7 +62,18 @@
 		xmlhttp.send();
 	}
 
-	function Item(value,remove_callback, is_dub) {
+
+
+	function onsave(opt) {
+		console.log("saved");//TODO: make user respose
+		chrome.runtime.sendMessage({cmd: "refresh_options", opt: str}, function(response) {
+		});
+	}
+
+
+
+	function Item(value, owner, warn) {
+		var self = {};
 		var dom = {
 			item		: Elm('div',	{cls:'item'}),
 			left_block	: Elm('div',	{cls:'item_left_block'}),
@@ -109,15 +92,10 @@
 			uid:'',
 			screen_name:'',
 			photo:'',
-			ok:false
 		};
 
-		var url = function() {
-			var url = 'http://api.vk.com/method/users.get?uids=';
-			url+=model.id;
-			url+='&fields=uid,first_name,last_name,screen_name,photo';
-			return url;
-		}
+
+
 
 		function init_view() {
 			dom.left_block.appendChild(dom.id_input);
@@ -129,253 +107,339 @@
 
 			dom.item.appendChild(dom.left_block);
 			dom.item.appendChild(dom.right_block);
-			dom.item.style.display='none';
+			display_none(dom.item);
 		}
 
 		function init_events() {
-			dom.remove_btn.onclick = remove;
-			dom.id_input.onchange = change;
+			dom.remove_btn.onclick = self.remove;
+			dom.id_input.onchange = self.change;
 		}
 
-		function splash() {
-			alarm(dom.id_input);
-		}
-
-		function error() {
-			model.ok = false;
-			splash();
-			reset_model();
-			render_model();
-		}
-
-		function remove() {
+		self.remove = function () {
+			console.log(self);
 			dom.item.remove();
-			remove_callback(this);
+			owner.onremove(self);
 		}
 
-		function change() {
+		self.change = function() {
 			model.id = dom.id_input.value;
-			refresh();
+			self.refresh_model();
 		}
 
-		function show() {
-			dom.item.style.display='block';
+		self.refresh_model = function() {
+			if(owner.have_dublicate(self)) {
+				model.have_dublicate=true;
+				self.render_model();
+			}
+			else {
+				var url = 'http://api.vk.com/method/users.get?uids=';
+				url+=model.id;
+				url+='&fields=uid,first_name,last_name,screen_name,photo';
+				ajax(url, self.validate_ajax, self.ajax_fail);
+			}
+		}
+
+		self.validate_ajax = function(resp) {
+			var r = resp;
+			model.api_error = r.error || r.response.length == 0;
+			if(!model.api_error) {
+				model.user_deactivated = r.response[0].deactivated;
+			}
+			if(model.api_error || model.user_deactivated) {
+				self.render_model();
+			}
+			else {
+				self.parse(r.response[0]);
+			}
+		}
+
+		self.ajax_fail = function(text) {
+			model.ajax_error = text;
+			self.render_model();
+		}
+
+		self.parse = function(resp) {
+			model.first_name = resp.first_name;
+			model.last_name = resp.last_name;
+			model.uid = resp.uid;
+			model.screen_name = resp.screen_name;
+			model.photo = resp.photo;
+			self.reset_model_errors();
+			self.render_model();
+		}
+
+		self.model_ok = function() {
+			return	model.ajax_error == '' &&
+					!model.api_error &&
+					!model.user_deactivated &&
+					!model.have_dublicate;
+		}
+
+		self.reset_model_errors = function() {
+			model.ajax_error = '';
+			model.api_error = false;
+			model.user_deactivated = undefined;
+			model.have_dublicate = false;
+		}
+
+		self.flash_green = function() {
+			flash_green(dom.id_input);
+		}
+
+		self.flash_red = function() {
+			flash_red(dom.id_input);
+		}
+
+		self.render_model = function() {
+			console.log('render_model');
+			if(self.model_ok()) {
+				dom.id_input.value		= model.id;
+				dom.user_pic.src		= model.photo;
+				dom.user_fio.innerText	= model.first_name + ' ' + model.last_name;
+				self.flash_green();
+				warn.ok(self);
+			}
+			else {
+				dom.user_pic.src		= '';
+				dom.user_fio.innerText	= '';
+				self.flash_red();
+				warn.error(self);
+			}
+			console.log(model,self,self.value);
+			if(self.value) {
+				self.show(); //if loading from storage show only after reload
+				self.value = undefined;
+			}
+		}
+
+		self.show = function() {
+			display_block(dom.item);
+			owner.show(self);
 			dom.id_input.focus();
 		}
 
-		function validate(raw_data) {
-			var d = raw_data,
-				is_error = d.error ||
-				d.response.length == 0 ||
-				d.response[0].deactivated;
-			if(is_error) {
-				error();
-				return;
-			}
-			else {
-				d = d.response[0];
-				model.first_name = d.first_name;
-				model.last_name = d.last_name;
-				model.uid = d.uid;
-				model.screen_name = d.screen_name;
-				model.photo = d.photo;
-				model.ok = true;
-			}
-			render_model();
-		}
-
-		function reset_model() {
-			model.first_name='';
-			model.last_name='';
-			model.uid='';
-			model.screen_name='';
-			model.photo='';
-		}
-
-		function render_model() {
-			dom.id_input.value	= model.id;
-			dom.user_pic.src	= model.photo;
-			dom.user_fio.innerText = model.first_name + ' ' + model.last_name;
-		}
-
-		function refresh(callback) {
-			if(is_dub(model.id)) {
-				error();
-				return;
-			}
-			ajax(url(), validate);
-			if(callback) {
-				callback(this);
-			}
-		}
-
-		function get_id() {
+		self.get_id = function() {
 			return model.id;
 		}
 
-		function get_ok() {
-			return model.ok;
+		self.get_el = function() {
+			return dom.item;
 		}
 
-		function for_save() {
-			var t = {};
-			t.id = model.id;
-			t.screen_name = model.screen_name;
-			return t;
+		self.get_persist = function() {
+			var d = {}
+			d['id'] = model.id;
+			d['screen_name'] = model.screen_name;
+			return d;
 		}
 
 		init_view();
 		init_events();
+		self.reset_model_errors();
 
-		return {
-			render_model: render_model,
-			refresh:refresh,
-			show: show,
-			el: dom.item,
-			id: get_id,
-			ok: get_ok,
-			splash:splash,
-			for_save:for_save
-		};
-	}
-
-
-	function ItemCollection(lkick) {
-		var items = [],
-			el = ById('items');
-
-		function refresh_callback(elm) {
-			el.appendChild(elm.el);
-			lkick();
+		if(value) {
+			console.log(value);
+			self.value = value;
+			self.refresh_model();
+		}
+		else {
+			console.log('Empty item');
+			self.show();
 		}
 
-		function remove_callback(elm) {
-			var r = items.indexOf(elm);
+		return self;
+	}
+
+	function Warn() {
+		var self = {};
+		var el = ById('warning');
+
+		self.ok = function(item) {
+			console.log("ok ",item);
+			el.innerText = "ok";
+			el.classList.remove('hard-red-glow');
+			el.classList.add('hard-green-glow');
+		}
+
+		self.error = function(item) {
+			console.log("error ",item);
+			el.innerText = "error";
+			el.classList.remove('hard-green-glow');
+			el.classList.add('hard-red-glow');
+		}
+
+		self.ok_text = function(text) {
+			el.innerText = text;
+			el.classList.remove('hard-red-glow');
+			el.classList.add('hard-green-glow');
+		}
+
+		self.text = function(text) {
+			el.classList.remove('hard-red-glow');
+			el.classList.remove('hard-green-glow');
+			el.innerText = text;
+		}
+
+		self.error_text = function(text) {
+			el.innerText = text;
+			el.classList.remove('hard-green-glow');
+			el.classList.add('hard-red-glow');
+		}
+
+		return self;
+	}
+
+	function StorageManager(warn) {
+		var self = {};
+
+		self.get = function(key, onready) {
+			chrome.storage.sync.get(key, function(o) {
+				var opt = JSON.parse(o[key]);
+				if(!opt) {
+					warn.error(self);
+				}
+				else {
+					warn.ok(self);
+				}
+				onready(opt);
+			});
+		}
+
+		self.set = function(key, value, onready) {
+			var opt = JSON.stringify(value);
+			if(!opt) {
+				warn.error(self);
+			}
+			var d = {};
+			d[key] = opt;
+			chrome.storage.sync.set(d, function() {
+				warn.ok(self);
+				onready();
+			});
+		}
+
+		return self;
+	}
+
+	function ItemCollection(app, storage, warn) {
+		var self = {};
+		var items = [],
+			el = ById('items')
+			persist_hash = 'options';
+
+		self.onremove = function(item) {
+			var r = items.indexOf(item);
 			items.splice(r,1);
 		}
 
-		function is_dublicate(id) {
-			var cnt = 0;
-			if(!id) return false;
+		self.have_dublicate = function (item) {
 			for(var i=0;i<items.length;i++) {
-				if(items[i].id() == id) {
-					if(cnt) return true;
-					cnt++;
+				if(items[i] != item) {
+					if(items[i].get_id() == item.get_id() ) return true;
 				}
 			}
 			return false;
 		}
 
-		function show_all() {
-			for(var i=0;i<items.length;i++) {
-				items[i].show();
-			}
-		}
-
-		function add(value) {
-			var i = Item(value, remove_callback, is_dublicate);
-			items.push(i);
-			if(value) {
-				i.refresh(refresh_callback);
-			}
-			else {
-				el.appendChild(i.el);
-				refresh_callback(i);
-				i.show();
-			}
-		}
-
-		function for_save() {
-			var result=[];
-			var e;
-			var ok = true;
-			for(var i=0;i<items.length;i++) {
-				e = items[i];
-				if(!e.ok()) {
-					e.splash();
-					ok = false;
+		self.show = function(item) {
+			el.appendChild(item.get_el());
+			if(self.n) {
+				self.n--;
+				if(self.n == 0) {
+					display_block(el);
+					app.enable_btns();
+					warn.ok_text('Loaded!');
 				}
-				result.push(e.for_save());
-			}
-			if(!ok) return '';
-			return JSON.stringify(result);
-		}
-
-		return {
-			add : add,
-			show_all: show_all,
-			for_save:for_save
-		};
-
-	}
-
-	function ProgressBar(cnt, callback) {
-		var cnt = cnt,
-		el  = ById('progress');
-
-		if(!cnt) {
-			el.style.display = 'none';
-		}
-
-		function kick() {
-			if(!cnt) return;
-			cnt--;
-			if(!cnt) {
-				el.style.display = 'none';
-				callback();
 			}
 		}
 
-		return {
-			kick:kick,
-		};
-	}
-
-
-	function AppController() {
-		var save = ById('savebtn')
-			add  = ById('addbtn'),
-			ic	 = ItemCollection(kick);
-
-		var pb;
-		function kick() {
-			console.log("kick");
-			pb.kick();
-		}
-		function show() {
-			console.log("show");
-			ic.show_all();
+		self.add = function (value) {
+			var item = Item(value, self, warn);
+			items.push(item);
 		}
 
-		function save_success() {
-			ok_alarm(save);
+		self.fetch = function() {
+			warn.text('Loading...');
+			storage.get(persist_hash,self.fetch_ready);
 		}
 
-		save.onclick = function() {
-			var str = ic.for_save();
-			console.log(str);
-			if(str) {
-				save_options(str, save_success);
-				saved=true;
+		self.fetch_ready = function(opt) {
+			self.n = opt.length;;
+			console.log('fetch_ready', opt, self.n,self);
+			for(var i=0;i<self.n;i++) {
+				console.log("here");
+				self.add(opt[i].id);
+			}
+			display_none(el);
+		}
+
+		self.save = function() {
+			var m,no_err=true,forsave=[];
+
+			for(var i=0;i<items.length;i++) {
+				m=items[i];
+				if(!m.get_id() || !m.model_ok()) {
+					no_err = false;
+					m.flash_red();
+				}
+				else {
+					m.flash_green();
+					forsave.push(m.get_persist());
+				}
+			}
+			if(no_err) {
+				app.disable_btns();
+				storage.set(persist_hash,forsave,self.save_ready);
 			}
 			else {
-				alarm(save);
+				warn.error_text('Save failed');
 			}
 		}
 
-		add.onclick = function() {
-			ic.add();
+		self.save_ready = function() {
+			console.log('save ready');
+			app.enable_btns();
+			warn.ok_text('Saved');
 		}
 
-		restore_options(function(opt) {
-			pb = ProgressBar(opt.length, show);
-			for(var i=0;i<opt.length;i++) {
-				ic.add(opt[i].id);
-			}
-		});
+		return self;
 	}
 
-	var App = AppController();
+
+	function App() {
+		var self = {};
+		self.save = ById('savebtn');
+		self.add  = ById('addbtn');
+
+		self.enable_btns = function() {
+			self.save.onclick = self.onsave;
+			self.add.onclick = self.onadd;
+		}
+
+		self.disable_btns = function() {
+			self.save.onclick = '';
+			self.add.onclick = '';
+		}
+
+		self.onsave =  function() {
+			self.ic.save();
+		}
+
+		self.onadd = function() {
+			self.ic.add('');
+		}
+
+		self.run = function() {
+			self.warn = Warn();
+			self.storage = StorageManager(self.warn);
+			self.ic = ItemCollection(self,self.storage,self.warn);
+			self.ic.fetch();
+		}
+		return self;
+	}
+
+	var app = App();
+	app.run();
 })();
 
 
